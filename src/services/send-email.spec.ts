d3dbdb549684896eb6f0090d9c99947233ce7b5e
DevWedeloper <vicnathangabrielle@gmail.com>
2025-03-19 14:25:51 +0800

    test: create test for sendEmail

---
+++ src/services/send-email.spec.ts
@@ -0,0 +1,34 @@
+import { describe, expect, it, mock, spyOn } from 'bun:test';
+import { sendEmail } from './send-email';
+
+describe('sendEmail', () => {
+  it('should send an email successfully', async () => {
+    const consoleLogSpy = spyOn(console, 'log');
+
+    mock.module('nodemailer', () => ({
+      createTransport: () => ({
+        sendMail: mock(() => Promise.resolve('Email sent')),
+      }),
+    }));
+
+    await sendEmail();
+    expect(consoleLogSpy).toHaveBeenCalledWith('Email sent: Commit reminder');
+  });
+
+  it('should log an error if email sending fails', async () => {
+    const consoleErrorSpy = spyOn(console, 'error');
+
+    mock.module('nodemailer', () => ({
+      createTransport: () => ({
+        sendMail: mock(() => Promise.reject(new Error('SMTP Error'))),
+      }),
+    }));
+
+    await sendEmail();
+
+    expect(consoleErrorSpy).toHaveBeenCalledWith(
+      expect.anything(),
+      expect.any(Error)
+    );
+  });
+});
