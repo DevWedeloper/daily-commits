e6595a8569e10ab5c1de43f048bff8ff3eb173f9
DevWedeloper <vicnathangabrielle@gmail.com>
2025-03-19 10:34:14 +0800

    refactor: reduce code duplication

--- src/services/auto-commit.ts
+++ src/services/auto-commit.ts
@@ -1,20 +1,20 @@
 import fs from 'fs';
 import simpleGit from 'simple-git';
 
+const getFormattedTimestamp = () => new Date().toUTCString();
+
 export const autoCommitAndPush = async () => {
   try {
     const git = simpleGit();
+    const timestamp = getFormattedTimestamp();
 
-    fs.appendFileSync(
-      'commit_log.txt',
-      `Automated commit on ${new Date().toUTCString()}\n`
-    );
+    fs.appendFileSync('commit_log.txt', `Automated commit on ${timestamp}\n`);
 
     await git.addConfig('user.name', 'DevWedeloper');
     await git.addConfig('user.email', 'vicnathangabrielle@gmail.com');
 
     await git.add('commit_log.txt');
-    await git.commit(`Automated commit on ${new Date().toUTCString()}`);
+    await git.commit(`Automated commit on ${timestamp}`);
     await git.push();
 
     console.log('Changes have been pushed successfully.');
