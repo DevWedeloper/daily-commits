271a4b16302f20c64bf78ebf4801182fadeb233e
DevWedeloper <vicnathangabrielle@gmail.com>
2026-02-05 05:14:23 +0800

    refactor: .txt -> .md

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const LOG_FILE = 'commit_log.txt'
+export const LOG_FILE = 'commit_log.md'
 export const GIT_USER_NAME = 'DevWedeloper'
 export const GIT_USER_EMAIL = 'vicnathangabrielle@gmail.com'
